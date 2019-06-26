m5d215/netspeeed

commit a1ab26217007c7180aa476df40d88d357906150b

Create hourly chart

src/modules/saga.ts
@@ -2,10 +2,9 @@ import axios, { AxiosResponse } from 'axios'
 import { SagaIterator } from 'redux-saga'
 import { call, delay, put, select, takeLatest } from 'redux-saga/effects'
 import { ActionType } from 'typesafe-actions'
-import NetworkSpeed, { smooth } from '../entities/NetworkSpeed'
+import NetworkSpeed from '../entities/NetworkSpeed'
 import Settings, { defaultSettings } from '../entities/Settings'
 import { transformRange } from '../entities/Viewport'
-import floorDate from '../utilities/floorDate'
 import { changeSettings, changeWindow, fetch, initialize } from './actions'
 import State from './State'
 
@@ -59,7 +58,7 @@ function* handleFetch(action: ActionType<typeof fetch.request>): SagaIterator {
       }
     )
     if (resolution !== 'day') {
-      yield put(fetch.success(smooth(data, floorDate)))
+      yield put(fetch.success(data))
     } else {
       yield put(fetch.success(data))
     }

src/App.tsx
@@ -2,6 +2,7 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import React, { FC } from 'react'
 import Navigation from './containers/Navigation'
 import NetworkSpeedChart from './containers/NetworkSpeedChart'
+import PerTimeChart from './containers/PerTimeChart'
 import ReferenceDatePicker from './containers/ReferenceDatePicker'
 
 const App: FC = () => {
@@ -11,6 +12,7 @@ const App: FC = () => {
       <Navigation />
       <ReferenceDatePicker />
       <NetworkSpeedChart />
+      <PerTimeChart />
     </>
   )
 }

src/components/PerTimeChart.tsx
@@ -1,0 +1,65 @@
+import { Theme } from '@material-ui/core'
+import blue from '@material-ui/core/colors/blue'
+import yellow from '@material-ui/core/colors/yellow'
+import { useTheme } from '@material-ui/styles'
+import dayjs from 'dayjs'
+import React, { FC, useMemo } from 'react'
+import {
+  PolarAngleAxis,
+  PolarGrid,
+  Radar,
+  RadarChart,
+  ResponsiveContainer
+} from 'recharts'
+import NetworkSpeed from '../entities/NetworkSpeed'
+import { average, groupBy } from '../utilities/collection'
+
+interface Props {
+  records: NetworkSpeed[]
+}
+
+const PerTimeChart: FC<Props> = props => {
+  const { records } = props
+
+  const theme = useTheme<Theme>()
+  const tick = useMemo<React.CSSProperties>(() => {
+    return {
+      fill: theme.palette.divider
+    }
+  }, [theme])
+
+  const m = groupBy(records, ({ timestamp }) => dayjs(timestamp).hour())
+
+  const data = Array.from(m.entries())
+    .sort((a, b) => a[0] - b[0])
+    .map(([hour, items]) => {
+      return {
+        hour,
+        upload: average(items, record => record.upload),
+        download: average(items, record => record.download)
+      }
+    })
+
+  return (
+    <ResponsiveContainer>
+      <RadarChart data={data}>
+        <PolarGrid stroke={theme.palette.divider} />
+        <PolarAngleAxis dataKey="hour" tick={tick} />
+        <Radar
+          dataKey="download"
+          stroke={blue[500]}
+          fill={blue[500]}
+          fillOpacity={0.6}
+        />
+        <Radar
+          dataKey="upload"
+          stroke={yellow[500]}
+          fill={yellow[500]}
+          fillOpacity={0.6}
+        />
+      </RadarChart>
+    </ResponsiveContainer>
+  )
+}
+
+export default PerTimeChart

src/containers/NetworkSpeedChart.tsx
@@ -1,6 +1,8 @@
 import { connect } from 'react-redux'
 import NetworkSpeedChart from '../components/NetworkSpeedChart'
+import { smooth } from '../entities/NetworkSpeed'
 import State from '../modules/State'
+import floorDate from '../utilities/floorDate'
 
 export default connect(
   (state: State) => {
@@ -8,7 +10,10 @@ export default connect(
       records,
       viewport: { resolution }
     } = state
-    return { records, resolution }
+    return {
+      records: resolution !== 'day' ? smooth(records, floorDate) : records,
+      resolution
+    }
   },
   {}
 )(NetworkSpeedChart)

src/containers/PerTimeChart.tsx
@@ -1,0 +1,11 @@
+import { connect } from 'react-redux'
+import PerTimeChart from '../components/PerTimeChart'
+import State from '../modules/State'
+
+export default connect(
+  (state: State) => {
+    const { records } = state
+    return { records }
+  },
+  {}
+)(PerTimeChart)

commit 4b29580cf31c8ce93d5024c00d3adc150a186005

Remove redundant code caused by a1ab262

src/modules/saga.ts
@@ -44,8 +44,7 @@ function* handleFetch(action: ActionType<typeof fetch.request>): SagaIterator {
   } = action
 
   const {
-    settings: { serverURL: baseURL, user },
-    viewport: { resolution }
+    settings: { serverURL: baseURL, user }
   }: State = yield select()
 
   try {
@@ -57,11 +56,7 @@ function* handleFetch(action: ActionType<typeof fetch.request>): SagaIterator {
         params: { begin, end }
       }
     )
-    if (resolution !== 'day') {
-      yield put(fetch.success(data))
-    } else {
-      yield put(fetch.success(data))
-    }
+    yield put(fetch.success(data))
   } catch (error) {
     yield put(fetch.failure(error))
   }